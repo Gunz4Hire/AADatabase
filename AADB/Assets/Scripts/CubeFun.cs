5643b289b53c69ff8123bcf8643d3e28213aed8b
Gunz4Hire <40991103+Gunz4Hire@users.noreply.github.com>
2021-03-09 11:12:50 -0800

    Added Image Effects and a simple cube test

---
+++ AADB/Assets/Scripts/CubeFun.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeFun : MonoBehaviour {
+
+	private Vector3 _startPosition;
+	float y;
+
+	void Start ()
+    {
+		y = Random.Range(.0f, .04f);
+	}
+	void Update () 
+	{
+		var rand = Random.Range(Mathf.Sin(Time.time), Mathf.Sin(Time.time * 5));
+		_startPosition = transform.position;
+		transform.position = _startPosition + new Vector3(Mathf.Sin(Time.time) * y, rand * y, Mathf.Sin(Time.time) * y);
+
+		transform.Rotate(Vector3.right);
+	}
+}
--- AADB/Assets/Scripts/xenonbuildlist.cs
+++ AADB/Assets/Scripts/xenonbuildlist.cs
@@ -5,11 +5,9 @@ using UnityEngine.Networking;
 
 public class xenonbuildlist : MonoBehaviour
 {
-    private string textFromWWW;
-    public Text siteText;
+    public Text buildListTxtElement;
     private string url = "https://wiki.alphaarchive.net/Builds.txt";
-    //public Text siteTextb;
-    //private string urlb = "https://wiki.alphaarchive.net/Builds2.txt";
+
 
     void Start()
     {
@@ -18,17 +16,16 @@ public class xenonbuildlist : MonoBehaviour
     IEnumerator GetTextFromWWW()
     {
         WWW www = new WWW(url);
-        //WWW wwwb = new WWW(urlb);
+
         yield return www;
-        //yield return wwwb;
+
         if (www.error != null)
         {
             Debug.Log("Ooops, something went wrong...");
         }
         else
         {
-            //siteTextb.text = wwwb.text;
-            siteText.text = www.text;
+            buildListTxtElement.text = www.text;
         }
     }
     void Update()
