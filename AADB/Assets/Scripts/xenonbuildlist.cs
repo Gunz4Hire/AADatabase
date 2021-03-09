d6fe4b36634c40282b22776ec4d5560d105a9695
Gunz4Hire <40991103+Gunz4Hire@users.noreply.github.com>
2021-03-09 10:21:51 -0800

    Add Initial Project

---
+++ AADB/Assets/Scripts/xenonbuildlist.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class xenonbuildlist : MonoBehaviour
+{
+    private string textFromWWW;
+    public Text siteText;
+    private string url = "https://wiki.alphaarchive.net/Builds.txt";
+    //public Text siteTextb;
+    //private string urlb = "https://wiki.alphaarchive.net/Builds2.txt";
+
+    void Start()
+    {
+        StartCoroutine(GetTextFromWWW());
+    }
+    IEnumerator GetTextFromWWW()
+    {
+        WWW www = new WWW(url);
+        //WWW wwwb = new WWW(urlb);
+        yield return www;
+        //yield return wwwb;
+        if (www.error != null)
+        {
+            Debug.Log("Ooops, something went wrong...");
+        }
+        else
+        {
+            //siteTextb.text = wwwb.text;
+            siteText.text = www.text;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
